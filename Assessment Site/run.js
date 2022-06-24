bc8a0783f49a08a692fd3d5634d19b37e575ef96
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2022-06-24 18:21:54 +1200

    Added file structure for node.js. StyleSheets and scripts not currently linked

---
+++ Assessment Site/run.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const index_router = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static('assets'))
+app.use("/", index_router)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
